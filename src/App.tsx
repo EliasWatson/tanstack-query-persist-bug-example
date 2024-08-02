818e96c91b1f326a34d7508441a12ed0b6e247d4
Elias Watson <ewatson@visiostack.com>
2024-08-02 11:52:02 -0400

    Remove `any` type

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { type ReactNode } from "react";
-import { queryOptions, QueryPersister } from "@tanstack/react-query";
+import {QueryKey, queryOptions, QueryPersister} from "@tanstack/react-query";
 import { experimental_createPersister } from "@tanstack/query-persist-client-core";
 
-function createPersister<TData>(): QueryPersister<TData, any> {
+function createPersister<TData, TQueryKey extends QueryKey>(): QueryPersister<TData, TQueryKey> {
   return experimental_createPersister({ storage: undefined });
 }
 
@@ -11,14 +11,16 @@ function App(): ReactNode {
   queryOptions({
     queryKey: ['good-query'],
     queryFn: () => 'test',
-    persister: createPersister<string>(),
+    persister: createPersister<string, string[]>(),
   });
 
   // Type error when queryFn has arguments:
   queryOptions({
     queryKey: ['bad-query'],
+    // Ignoring: 'args' is declared but its value is never read.
+    // @ts-ignore
     queryFn: (args) => 'test',
-    persister: createPersister<string>(),
+    persister: createPersister<string, string[]>(),
   });
 
   return null;
