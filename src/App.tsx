170562dfed54da6353fb8f6ac670ea9cab9dd4ae
Elias Watson <ewatson@visiostack.com>
2024-08-02 11:44:45 -0400

    Add type error example

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,26 @@
 import { type ReactNode } from "react";
+import { queryOptions, QueryPersister } from "@tanstack/react-query";
+import { experimental_createPersister } from "@tanstack/query-persist-client-core";
+
+function createPersister<TData>(): QueryPersister<TData, any> {
+  return experimental_createPersister({ storage: undefined });
+}
 
 function App(): ReactNode {
+  // No type errors when queryFn has no arguments:
+  queryOptions({
+    queryKey: ['good-query'],
+    queryFn: () => 'test',
+    persister: createPersister<string>(),
+  });
+
+  // Type error when queryFn has arguments:
+  queryOptions({
+    queryKey: ['bad-query'],
+    queryFn: (args) => 'test',
+    persister: createPersister<string>(),
+  });
+
   return null;
 }
 
